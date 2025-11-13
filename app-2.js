911bd537f680e64b0fba4a040b2935e3f0ecaed5
bienvenudev <cbienvenu007@gmail.com>
2025-11-13 10:24:07 +0200

    feat: improved error handling and conversions

--- app-2.js
+++ app-2.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  let convertedAmount = null;
-  const amountQuery = req.query.amount;
+const conversion = {
+  usd: 1450,
+  eur: 1682,
+  gbp: 1907,
+};
+
+app.get("/convert", (req, res) => {
   const currencyQuery = req.query.currency;
 
-  console.log(amountQuery, currencyQuery);
+  if (!currencyQuery) return res.status(400).send("missing currency query!");
 
-  if (currencyQuery === "usd") {
-    convertedAmount = amountQuery * 1450;
-  } else if (currencyQuery === "eur") {
-    convertedAmount = amountQuery * 1682;
-  } else if (currencyQuery === "gbp") {
-    convertedAmount = amountQuery * 1907;
-  } else {
-    return res.status(400).send("missing amount or currency query!");
+  if (!conversion[currencyQuery])
+    return res
+      .status(400)
+      .send("invalid currency query. currencies available: usd, eur and gbp!");
+
+  if (!req.query.amount) return res.status(400).send("missing amount query!");
+
+  const amountQuery = Number(req.query.amount);
+
+  if (isNaN(amountQuery)) {
+    return res.status(400).send("invalid amount query!");
   }
+
+  if (amountQuery <= 0) {
+    return res.status(400).send("amount must be positive!");
+  }
+
+  const convertedAmount = amountQuery * conversion[currencyQuery];
+
   res.json({
     input: { amount: amountQuery, currency: currencyQuery },
     convertedAmount,
